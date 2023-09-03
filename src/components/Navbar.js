f9512b66d8ecbcd15477712316e7b465ddbda3e6
abhikunu007 <ak0879918@gmail.com>
2023-09-03 19:22:11 +0530

    create navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 export default function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">GrabIt</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,8 +14,10 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav"> 
         <Link className="nav-link " aria-current="page" to="/">Home</Link>
-        <Link className="nav-link " to="/">Login</Link>
-        <Link className="nav-link " to="/signup">Sign up</Link>
+       <div className="navauth">
+       <Link className="nav-link auth " to="/login">Login</Link>
+        <Link className="nav-link auth" to="/signup">Sign up</Link>
+       </div>
       </div>
     </div>
   </div>
