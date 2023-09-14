927ac6022d8716db71d30e615ae4696e3b93c215
abhikunu007 <ak0879918@gmail.com>
2023-09-14 22:25:56 +0530

    implement password encrypting

--- backend/Routes/Login.js
+++ backend/Routes/Login.js
@@ -3,82 +3,96 @@ const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
-// router.post("/login",
-// // Putting validation
-
-// body('email', 'Enter valid email').isEmail(),
-// body('password', 'Length must be greater than 7').isLength({min:7}),
-
-// async (req, res) => {
-
-//              // Check Validation
-//              const errors = validationResult(req);
-//              if(!errors.isEmpty())
-//              {
-//                  return res.status(400).json({errors: errors.array()});
-//              }
-
-//     let email = req.body.email;
-//    try{
-
-//     let usercred = await User.findOne({ email });
-//         if(!usercred)
-//         {
-//             return res.status(400).json({errors: "Try logging with valid credentials"});   
-//         }
-
-//         if(req.body.password !== usercred.password)
-//         {
-//             return res.status(400).json({errors: "Please enter valid password"});
-//         }
-
-//          return res.json({ success: true });
-
-//    }
-
-//     catch(error) {
-//         console.error(error);
-//         res.json({ success: false });
-//     }
-// });
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwtSecret = "hedsjbzjhb#hwj@efjisjdke###adhjkzxn";
 
 router.post("/login",
-    // Putting validation
+// Putting validation
 
-    [
-        body('email', 'Enter valid email').isEmail(),
-        body('password', 'Length must be greater than 7').isLength({ min: 7 }),
+body('email', 'Enter valid email').isEmail(),
+body('password', 'Length must be greater than 7').isLength({min:7}),
 
-    ],
-    (req, res) => {
+async (req, res) => {
 
-        // Check Validation
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+             // Check Validation
+             const errors = validationResult(req);
+             if(!errors.isEmpty())
+             {
+                 return res.status(400).json({errors: errors.array()});
+             }
+
+    let email = req.body.email;
+   try{
+
+    let usercred = await User.findOne({ email });
+        if(!usercred)
+        {
+            return res.status(400).json({errors: "Try logging with valid credentials"});   
         }
 
-        let email = req.body.email;
+        const pass = await bcrypt.compare(req.body.password, usercred.password)
 
-        User.findOne({ email })
-            .then(usercred => {
-                if (!usercred) {
-                    return res.status(400).json({ errors: "Try logging with valid credentials" });
-                }
+        if(!pass)
+        {
+            return res.status(400).json({errors: "Please enter valid password"});
+        }
 
-                if (req.body.password !== usercred.password) {
-                    return res.status(400).json({ errors: "Please enter valid password" });
-                }
+        const data = {
+            user:{
+                id: usercred.id
+            }
+        }
 
-                return res.json({ success: true });
-            })
+        const authToken = jwt.sign(data, jwtSecret)
 
+         return res.json({ success: true, authToken: authToken });
 
-            .catch(error => {
-                console.error(error);
-                res.json({ success: false });
-            });
+   }
+
+    catch(error) {
+        console.error(error);
+        res.json({ success: false });
     }
-);
+});
+
+// router.post("/login",
+//     // Putting validation
+
+//     [
+//         body('email', 'Enter valid email').isEmail(),
+//         body('password', 'Length must be greater than 7').isLength({ min: 7 }),
+
+//     ],
+//     (req, res) => {
+
+//         // Check Validation
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
+
+//         let email = req.body.email;
+
+//         User.findOne({ email })
+//             .then(usercred => {
+//                 if (!usercred) {
+//                     return res.status(400).json({ errors: "Try logging with valid credentials" });
+//                 }
+
+//                 if (req.body.password !== usercred.password) {
+//                     return res.status(400).json({ errors: "Please enter valid password" });
+//                 }
+
+//                 return res.json({ success: true });
+//             })
+
+
+//             .catch(error => {
+//                 console.error(error);
+//                 res.json({ success: false });
+//             });
+//     }
+// );
 
 module.exports = router;
