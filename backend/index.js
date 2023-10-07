5555a03bdd4f677c96e21ed865eea8850d4c9de4
abhikunu007 <ak0879918@gmail.com>
2023-10-07 20:09:38 +0530

    add myorder router

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,33 @@
+
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
 const mongoDB = require('./mongoose')
 mongoDB();
+
+
+
+app.use((req,res,next)=> {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.use(express.json());
-app.use('/api', require("./Routes/CreateUser"));
+app.use('/api', require("./Routes/Signup"));
+app.use('/api', require("./Routes/Login"));
+app.use('/api', require("./Routes/Items"));
+app.use('/api', require("./Routes/OrderData"));
+app.use('/api', require("./Routes/Myorder"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import { CartProvider } from './components/ContextReducer';
+import MyOrder from './components/MyOrder';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route exact path='/' element={<Home></Home>} />
           <Route exact path='/login' element={<Login></Login>} />
           <Route exact path='/signup' element={<Signup></Signup>} />
+          <Route exact path='/myorder' element={<MyOrder></MyOrder>} />
         </Routes>
       </div>
     </Router>
