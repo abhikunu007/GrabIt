4576fbcdd3a644e8ac09d0ca50a5f4456e27d810
abhikunu007 <ak0879918@gmail.com>
2023-10-07 20:05:57 +0530

    add image to myorder component card

---
+++ src/components/MyOrder.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+export default function MyOrder() {
+
+
+    const [orderData, setorderData] = useState({})
+
+    const fetchMyOrder = async () => {
+        // console.log(localStorage.getItem('userEmail'))
+        await fetch("http://localhost:5000/api/myorder", {
+            // credentials: 'include',
+            // Origin:"http://localhost:3000/login",
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: localStorage.getItem('userEmail')
+            })
+        }).then(async (res) => {
+            let response = await res.json()
+            await setorderData(response)
+        })
+
+
+
+        // await res.map((data)=>{
+        //    console.log(data)
+        // }) 
+
+
+    }
+
+    useEffect(() => {
+        fetchMyOrder()
+    }, [])
+
+
+    return (
+        <>
+            <div><Navbar></Navbar></div>
+
+            <div className='container'>
+                <div className='row'>
+
+                    {orderData != {} ? Array(orderData).map(data => {
+                        return (
+                            data.orderData ?
+                                data.orderData.order_data.slice(0).reverse().map((item) => {
+                                    return (
+                                        item.map((arrayData) => {
+                                            return (
+                                                <div  >
+                                                    {arrayData.Order_date ? <div className='m-auto mt-5'>
+
+                                                        {data = arrayData.Order_date}
+                                                        <hr />
+                                                    </div> :
+
+                                                        <div className='col-12 col-md-6 col-lg-3' >
+                                                            <div className="card mt-3" style={{ width: "18rem", maxHeight: "400px" }}>
+                                                                <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
+                                                                <div className="card-body">
+                                                                    <h5 className="card-title">{arrayData.name}</h5>
+                                                                    <div className='container w-100 p-0 fs-5' style={{ height: "60px" }}>
+                                                                        <span className=''>{arrayData.qty}</span>
+                                                                        <span className='m-1'>{arrayData.size}</span>
+                                                                        <span className='m-1'>{data}</span>
+                                                                        <div className='   h-100 w-10 fs-5' >
+                                                                            ₹{arrayData.price}/-
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+
+
+
+                                                    }
+
+                                                </div>
+                                            )
+                                        })
+
+                                    )
+                                }) : ""
+                        )
+                    }) : ""}
+                </div>
+
+
+            </div>
+
+
+            <div><Footer></Footer></div>
+        </>
+    )
+}
